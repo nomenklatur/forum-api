36a68a0c596005c8aba25ee9ef5fa506364a8c17
Dimas <dimasekaputra2000@gmail.com>
2024-09-02 21:46:36 +0700

    feat: thread repository

---
+++ src/Domains/threads/ThreadRepository.js
@@ -0,0 +1,16 @@
+class ThreadRepository {
+    async create(newThread) {
+      throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    }
+  
+    async isAvailable(thread) {
+      throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    }
+  
+    async getDetail(thread) {
+      throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    }
+  }
+  
+  module.exports = ThreadRepository;
+  
