98d6c7a980e90fb1ca228bd4f23f2933b845ae18
Dimas <dimasekaputra2000@gmail.com>
2025-05-31 23:18:02 +0700

    feat: create threads table

---
+++ migrations/1748707621483_create-table-threads.js
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(100)',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    date: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('CURRENT_TIMESTAMP'),
+    },
+  });
+
+  pgm.addConstraint('threads', 'fk_threads.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('threads', 'fk_threads.user_id_users.id');
+  pgm.dropTable('threads');
+};
