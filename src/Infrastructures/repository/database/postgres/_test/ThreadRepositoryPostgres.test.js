f0b20b98277408e441894d9cc800b6f2533e9e7d
Dimas <dimasekaputra2000@gmail.com>
2025-08-26 07:11:57 +0700

    feat: test 2

--- src/Infrastructures/repository/database/postgres/_test/ThreadRepositoryPostgres.test.js
+++ src/Infrastructures/repository/database/postgres/_test/ThreadRepositoryPostgres.test.js
@@ -138,7 +138,7 @@ describe('ThreadRepositoryPostgres', () => {
           content: 'This is a comment'
         },
         {
-          thread_id: 'thread-123',
+          thread_id: 'thread-1234',
           title: 'Thread Title',
           body: 'Thread Body',
           date: new Date(date),
